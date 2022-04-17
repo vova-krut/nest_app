313a8624e4cac1d96cbd46fa0d667041b209935c
Volodymyr <vthehang@gmail.com>
2022-04-17 14:01:39 +0300

    made sequalize model with env vars and created user model

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,27 @@
+import { User } from "./users/users.model";
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
 import { SequelizeModule } from "@nestjs/sequelize";
+import { UsersModule } from "./users/users.module";
 
 @Module({
     controllers: [],
     providers: [],
     imports: [
+        ConfigModule.forRoot({
+            envFilePath: `.${process.env.NODE_ENV}.env`,
+        }),
         SequelizeModule.forRoot({
             dialect: "postgres",
-            host: "localhost",
-            port: 5432,
-            username: "postgres",
-            password: "1234",
-            database: "nest_app",
-            models: [],
+            host: process.env.POSTGRES_HOST,
+            port: Number(process.env.POSTGRES_PORT),
+            username: process.env.POSTGRES_USER,
+            password: process.env.POSTGRES_PASSWORD,
+            database: process.env.POSTGRES_DB,
+            models: [User],
             autoLoadModels: true,
         }),
+        UsersModule,
     ],
 })
 export class AppModule {}
