03ae8db1dd13b25f0cdbd7c3650afb6ef261ad48
Volodymyr <vthehang@gmail.com>
2022-04-17 14:17:25 +0300

    finished base API (add users, get allUsers)

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { UsersService } from "./users.service";
+import { CreateUserDto } from "./dto/create-user.dto";
+import { Body, Controller, Get, Post } from "@nestjs/common";
 
-@Controller('users')
-export class UsersController {}
+@Controller("users")
+export class UsersController {
+    constructor(private userService: UsersService) {}
+
+    @Post()
+    create(@Body() userDto: CreateUserDto) {
+        return this.userService.createUser(userDto);
+    }
+
+    @Get()
+    getAll() {
+        return this.userService.getAllUsers();
+    }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from "./dto/create-user.dto";
+import { InjectModel } from "@nestjs/sequelize";
+import { User } from "./users.model";
+import { Injectable } from "@nestjs/common";
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(@InjectModel(User) private userRepository: typeof User) {}
+
+    async createUser(dto: CreateUserDto) {
+        const user = await this.userRepository.create(dto);
+        return user;
+    }
+
+    async getAllUsers() {
+        const users = await this.userRepository.findAll();
+        return users;
+    }
+}
