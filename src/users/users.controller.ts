f05bbf2bbb7058701f806c1a135c508b44257f4e
Volodymyr <vthehang@gmail.com>
2022-04-19 13:42:31 +0300

    Added roleGuards and Role Decorator for endpoints

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,11 @@
+import { RolesGuard } from "./../auth/roles.guard";
 import { User } from "./users.model";
 import { UsersService } from "./users.service";
 import { CreateUserDto } from "./dto/create-user.dto";
 import { Body, Controller, Get, Post, UseGuards } from "@nestjs/common";
 import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 import { JwtAuthGuard } from "src/auth/jwt-auth.guard";
+import { Roles } from "src/auth/roles-auth.decorator";
 
 @ApiTags("Users")
 @Controller("users")
@@ -19,6 +21,8 @@ export class UsersController {
 
     @ApiOperation({ summary: "Get all users" })
     @ApiResponse({ status: 200, type: [User] })
+    @Roles("ADMIN")
+    @UseGuards(RolesGuard)
     @UseGuards(JwtAuthGuard)
     @Get()
     getAll() {
